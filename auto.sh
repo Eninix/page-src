0e0e5af16a3577d2118b302e41e3ee84d5f8e6af
Eninix <enin.ix@qq.com>
2022-11-11 02:06:06 +0800

    auto update by script

---
+++ auto.sh
@@ -0,0 +1,8 @@
+echo '=======>hexo generate'
+hexo g
+echo '=======>hexo deploy'
+hexo d
+echo '=======>push to github'
+SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE[0]}); pwd)
+cd $SCRIPT_DIR
+./push-to-github.sh
