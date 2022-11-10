c0798db69cdbcae0ec66fe6dc368399165489752
Eninix <enin.ix@qq.com>
2022-11-11 00:27:13 +0800

    auto update by script

--- push-to-github.sh
+++ push-to-github.sh
@@ -1,8 +1,11 @@
+echo '==> Prepare to Push'
 dirpath=`pwd`
 cd $dirpath
 git add .
 git commit -m 'auto update by script'
+
 echo '==> Start Push'
 git push
+echo '==> Push Complete'
 echo ''
 echo 'Done.'
